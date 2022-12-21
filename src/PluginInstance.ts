c4609a3c1c4a0fd8c992066c2e5befcd9da0275d
Syed Yaseen Abbas Rizvi <yaseenr@geekyants.com>
2022-12-21 11:52:35 +0530

    v0.0.1

---
+++ src/PluginInstance.ts
@@ -0,0 +1,59 @@
+import IApp from "@gluestack/framework/types/app/interface/IApp";
+import IPlugin from "@gluestack/framework/types/plugin/interface/IPlugin";
+import IInstance from "@gluestack/framework/types/plugin/interface/IInstance";
+import ILifeCycle from "@gluestack/framework/types/plugin/interface/ILifeCycle";
+import IGlueStorePlugin from "@gluestack/framework/types/store/interface/IGluePluginStore";
+import IContainerController from "@gluestack/framework/types/plugin/interface/IContainerController";
+import IHasContainerController from "@gluestack/framework/types/plugin/interface/IHasContainerController";
+import { PluginInstanceContainerController } from "./PluginInstanceContainerController";
+
+export class PluginInstance
+  implements IInstance, ILifeCycle, IHasContainerController {
+  app: IApp;
+  name: string;
+  callerPlugin: IPlugin;
+  containerController: IContainerController;
+  isOfTypeInstance: boolean = false;
+  gluePluginStore: IGlueStorePlugin;
+  installationPath: string;
+
+  constructor(
+    app: IApp,
+    callerPlugin: IPlugin,
+    name: string,
+    gluePluginStore: IGlueStorePlugin,
+    installationPath: string,
+  ) {
+    this.app = app;
+    this.name = name;
+    this.callerPlugin = callerPlugin;
+    this.gluePluginStore = gluePluginStore;
+    this.installationPath = installationPath;
+    this.containerController = new PluginInstanceContainerController(app, this);
+  }
+
+  init() {
+    //
+  }
+
+  destroy() {
+    //
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getCallerPlugin(): IPlugin {
+    return this.callerPlugin;
+  }
+
+  getInstallationPath(): string {
+    return this.installationPath;
+  }
+
+  getContainerController(): IContainerController {
+    return this.containerController;
+  }
+
+}
---
+++ src/PluginInstanceContainerController.ts
@@ -0,0 +1,187 @@
+const { DockerodeHelper } = require("@gluestack/helpers");
+import IApp from "@gluestack/framework/types/app/interface/IApp";
+import IInstance from "@gluestack/framework/types/plugin/interface/IInstance";
+import IContainerController from "@gluestack/framework/types/plugin/interface/IContainerController";
+
+export class PluginInstanceContainerController implements IContainerController {
+  app: IApp;
+  status: "up" | "down" = "down";
+  portNumber: number;
+  containerId: string;
+  dockerfile: string;
+  callerInstance: IInstance;
+
+  constructor(app: IApp, callerInstance: IInstance) {
+    this.app = app;
+    this.callerInstance = callerInstance;
+    this.setStatus(this.callerInstance.gluePluginStore.get("status"));
+    this.setPortNumber(this.callerInstance.gluePluginStore.get("port_number"));
+    this.setContainerId(
+      this.callerInstance.gluePluginStore.get("container_id"),
+    );
+  }
+
+  getCallerInstance(): IInstance {
+    return this.callerInstance;
+  }
+
+  getEnv() {
+    let pg_config = {
+      email: "admin@gluestack.io",
+      password: "password",
+    };
+
+    if (!this.callerInstance.gluePluginStore.get("pg_config") || !this.callerInstance.gluePluginStore.get("pg_config").email)
+      this.callerInstance.gluePluginStore.set("pg_config", pg_config);
+
+    pg_config = this.callerInstance.gluePluginStore.get("pg_config");
+
+    return {
+      PGADMIN_DEFAULT_EMAIL: pg_config.email,
+      PGADMIN_DEFAULT_PASSWORD: pg_config.password,
+    };
+  }
+
+  getDockerJson() {
+    return {
+      Image: "dpage/pgadmin4",
+      HostConfig: {
+        PortBindings: {
+          "80/tcp": [
+            {
+              HostPort: this.getPortNumber(true).toString(),
+            },
+          ],
+        },
+      },
+      ExposedPorts: {
+        "80/tcp": {},
+      }
+    };
+  }
+
+
+  getStatus(): "up" | "down" {
+    return this.status;
+  }
+
+  getPortNumber(returnDefault?: boolean): number {
+    if (this.portNumber) {
+      return this.portNumber;
+    }
+    if (returnDefault) {
+      return 5050;
+    }
+  }
+
+  getContainerId(): string {
+    return this.containerId;
+  }
+
+  setStatus(status: "up" | "down") {
+    this.callerInstance.gluePluginStore.set("status", status || "down");
+    return (this.status = status || "down");
+  }
+
+  setPortNumber(portNumber: number) {
+    this.callerInstance.gluePluginStore.set("port_number", portNumber || null);
+    return (this.portNumber = portNumber || null);
+  }
+
+  setContainerId(containerId: string) {
+    this.callerInstance.gluePluginStore.set(
+      "container_id",
+      containerId || null,
+    );
+    return (this.containerId = containerId || null);
+  }
+
+  setDockerfile(dockerfile: string) {
+    this.callerInstance.gluePluginStore.set("dockerfile", dockerfile || null);
+    return (this.dockerfile = dockerfile || null);
+  }
+
+  getConfig(): any { }
+
+  async up() {
+    let ports =
+      this.callerInstance.callerPlugin.gluePluginStore.get("ports") || [];
+
+    await new Promise(async (resolve, reject) => {
+      DockerodeHelper.getPort(this.getPortNumber(true), ports)
+        .then((port: number) => {
+          this.portNumber = port;
+          DockerodeHelper.up(
+            this.getDockerJson(),
+            this.getEnv(),
+            this.portNumber,
+            this.callerInstance.getName(),
+          )
+            .then(
+              ({
+                status,
+                portNumber,
+                containerId,
+              }: {
+                status: "up" | "down";
+                portNumber: number;
+                containerId: string;
+                dockerfile: string;
+              }) => {
+                DockerodeHelper.generateDockerFile(this.getDockerJson(),
+                  this.getEnv(),
+                  this.callerInstance.getName())
+                this.setStatus(status);
+                this.setPortNumber(portNumber);
+                this.setContainerId(containerId);
+                ports.push(portNumber);
+                this.callerInstance.callerPlugin.gluePluginStore.set(
+                  "ports",
+                  ports,
+                );
+                console.log("\x1b[32m");
+                console.log(`Open http://localhost:${this.getPortNumber()}/ in browser`);
+                console.log();
+                console.log(`Credentials to login in pgAdmin: `);
+                console.log(`email: ${this.getEnv().PGADMIN_DEFAULT_EMAIL}`);
+                console.log(`password: ${this.getEnv().PGADMIN_DEFAULT_PASSWORD}`);
+                console.log("\x1b[0m")
+                console.log();
+                return resolve(true);
+              },
+            )
+            .catch((e: any) => {
+              return reject(e);
+            });
+        })
+        .catch((e: any) => {
+          return reject(e);
+        });
+    });
+  }
+
+  async down() {
+    let ports =
+      this.callerInstance.callerPlugin.gluePluginStore.get("ports") || [];
+    await new Promise(async (resolve, reject) => {
+      DockerodeHelper.down(this.getContainerId(), this.callerInstance.getName())
+        .then(() => {
+          this.setStatus("down");
+          var index = ports.indexOf(this.getPortNumber());
+          if (index !== -1) {
+            ports.splice(index, 1);
+          }
+          this.callerInstance.callerPlugin.gluePluginStore.set("ports", ports);
+
+          this.setPortNumber(null);
+          this.setContainerId(null);
+          return resolve(true);
+        })
+        .catch((e: any) => {
+          return reject(e);
+        });
+    });
+  }
+
+  async build() { }
+}
